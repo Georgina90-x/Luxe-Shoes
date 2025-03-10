050c895deaf978891277478e3dc23c719da403ef
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-10 18:12:27 -0500

    added size logic to views & context

--- bag/views.py
+++ bag/views.py
@@ -12,12 +12,26 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    shoesize = None
+    if 'shoe_size' in request.POST:
+        shoesize = request.POST['shoe_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    """ Logic for adding shoe sizes to the shopping bag """
+    if shoesize:
+        if item_id in list(bag.keys()):
+            if shoesize in bag[item_id]['items_by_shoesize'].keys():
+                bag[item_id]['items_by_shoesize'][shoesize] += quantity
+            else:
+                bag[item_id]['items_by_shoesize'][shoesize] = quantity
+        else:
+            bag[item_id] = {'items_by_shoesize': {shoesize: quantity}}
     else:
-        bag[item_id] = quantity
+
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
