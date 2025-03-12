3a1101ff1c528029150cf8c523c68ceb048c7f89
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-11 22:06:44 -0500

    added stripe js

--- checkout/views.py
+++ checkout/views.py
@@ -4,17 +4,18 @@ from django.contrib import messages
 from .forms import OrderForm
 
 
-
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your bag is empty!")
         return redirect(reverse('products'))
-    
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QwYdIDlC1XVCtbH2pfqz1bsGqq5UO9A3HqQf9B5Jx89h9BIpqP8TViehGvrvwv8LRtMkB8LdXJLdipXF0YnKuN2000FCVXOtA',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
