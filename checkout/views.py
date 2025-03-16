d4d43d16c83fffb2125030207cf55d2b1b6998ef
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-16 15:49:12 -0500

    attempt at fixing checkout error in heroku

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,9 @@ from bag.contexts import bag_contents
 
 import stripe
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @require_POST
@@ -30,94 +33,98 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    try:
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    intent = None # added line may need to remove
+        intent = None # added line may need to remove
 
-    if request.method == 'POST':
-        bag = request.session.get('bag', {})
+        if request.method == 'POST':
+            bag = request.session.get('bag', {})
 
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'town_or_city': request.POST['town_or_city'],
-            'county': request.POST['county'],
-            'postcode': request.POST['postcode'],
-            'country': request.POST['country'],
-            'phone_number': request.POST['phone_number'],
-        }
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_bag = json.dumps(bag)
-            order.save()
-            for item_id, item_data in bag.items():
-                try:
-                    product = Product.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
-                        )
-                        order_line_item.save()
-                    else:
-                        for shoesize, quantity in item_data['items_by_shoesize'].items():
+            form_data = {
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+                'street_address1': request.POST['street_address1'],
+                'street_address2': request.POST['street_address2'],
+                'town_or_city': request.POST['town_or_city'],
+                'county': request.POST['county'],
+                'postcode': request.POST['postcode'],
+                'country': request.POST['country'],
+                'phone_number': request.POST['phone_number'],
+            }
+            order_form = OrderForm(form_data)
+            if order_form.is_valid():
+                order = order_form.save(commit=False)
+                pid = request.POST.get('client_secret').split('_secret')[0]
+                order.stripe_pid = pid
+                order.original_bag = json.dumps(bag)
+                order.save()
+                for item_id, item_data in bag.items():
+                    try:
+                        product = Product.objects.get(id=item_id)
+                        if isinstance(item_data, int):
                             order_line_item = OrderLineItem(
-                               order=order,
-                               product=product,
-                               quantity=quantity,
-                               shoe_size=shoesize,
+                                order=order,
+                                product=product,
+                                quantity=item_data,
                             )
                             order_line_item.save()
-                except Product.DoesNotExist:
-                    messages.error(request, (
-                        "One of the products in your bag wasn't found.\
-                              Please contact us for assistance.")
-                    )
-                    order.delete()
-                    return redirect(reverse('view_bag'))
+                        else:
+                            for shoesize, quantity in item_data['items_by_shoesize'].items():
+                                order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                shoe_size=shoesize,
+                                )
+                                order_line_item.save()
+                    except Product.DoesNotExist:
+                        messages.error(request, (
+                            "One of the products in your bag wasn't found.\
+                                Please contact us for assistance.")
+                        )
+                        order.delete()
+                        return redirect(reverse('view_bag'))
 
-            request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+                request.session['save_info'] = 'save-info' in request.POST
+                return redirect(reverse('checkout_success', args=[order.order_number]))
+            else:
+                messages.error(request, 'There was an error processing your form.\
+                            Please check you have input the correct details.')
+
         else:
-            messages.error(request, 'There was an error processing your form.\
-                           Please check you have input the correct details.')
+            bag = request.session.get('bag', {})
+            if not bag:
+                messages.error(request, "Your bag is empty!")
+                return redirect(reverse('products'))
 
-    else:
-        bag = request.session.get('bag', {})
-        if not bag:
-            messages.error(request, "Your bag is empty!")
-            return redirect(reverse('products'))
+            current_bag = bag_contents(request)
+            total = current_bag['grand_total']
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
 
-        current_bag = bag_contents(request)
-        total = current_bag['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
+            order_form = OrderForm()
 
-        order_form = OrderForm()
+            if not stripe_public_key:
+                messages.warning(request, 'Stripe public key is missing. \
+                            Did you forget to set it in your environment?')
 
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing. \
-                        Did you forget to set it in your environment?')
+            template = 'checkout/checkout.html'
+            context = {
+                'order_form': order_form,
+                'stripe_public_key': stripe_public_key,
+                'client_secret': intent.client_secret if intent else '',  # added line, may need to remove
+            }
 
-        template = 'checkout/checkout.html'
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret': intent.client_secret if intent else '',  # added line, may need to remove
-        }
-
-        return render(request, template, context)
+            return render(request, template, context)
+    except Exception as e:
+        logger.error(f"Error during checkout: {str(e)}")
+        raise e
 
 
 def checkout_success(request, order_number):
